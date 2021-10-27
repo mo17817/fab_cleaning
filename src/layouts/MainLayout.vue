833f4590d44840a3ea582fa0138bb6b4617e3ef8
austin4399 <Austin4399@gmail.com>
2021-10-27 15:19:19 -0400

    playing with navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,34 +2,44 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar class="bg-primary glossy text-white">
-        <q-breadcrumbs active-color="white" style="font-size: 17px" gutter ="xl">
-          <q-breadcrumbs-el
-            label="Home"
-            icon="home"
-            href="src/pages/index.vue"
-          />
-          <q-separator dark vertical inset />
-          <q-breadcrumbs-el
-            label="About"
-            icon="widgets"
-            href="src/pages/index.vue"
-          />
-          <q-separator dark vertical inset />
-          <q-breadcrumbs-el
-            label="Services"
-            icon="build"
-            href="src/pages/index.vue"
-          />
-          <!-- <q-separator dark vertical inset /> -->
-          <q-avatar size="70px">
-            <img
-              class="rounded-circle align-self-start mr-3"
-              src="../assets/FABlogo.png"
+          <div class="col">
+            <div class="row">
+                <q-avatar>
+                  <img
+                    class="rounded-circle align-self-start mr-3"
+                    src="../assets/FABlogo.png"
+                  />
+              </q-avatar>
+            </div>
+
+          </div>
+          <div class="col">
+            <q-toolbar-title align='center'>
+            FAB cleaning
+            </q-toolbar-title>
+          </div>
+
+          <div class="col flex-break">
+                     <q-breadcrumbs active-color="white" gutter ="lg" align="right">
+
+            <!-- <q-separator dark vertical inset /> -->
+            <q-breadcrumbs-el
+              label="About"
+              icon="widgets"
+              href="src/pages/index.vue"
             />
-          </q-avatar>
-        </q-breadcrumbs>
-        <q-toolbar-title>
-        </q-toolbar-title>
+            <!-- <q-separator dark vertical inset /> -->
+            <q-breadcrumbs-el
+              label="Services"
+              icon="build"
+              href="src/pages/index.vue"
+            />
+            <!-- <q-separator dark vertical inset /> -->
+            
+          </q-breadcrumbs>
+
+          </div>
+ 
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -57,52 +67,7 @@
       </q-list>
     </q-drawer> **commented out the drawer**-->
 <script>
-import EssentialLink from "components/EssentialLink.vue";
 
-const linksList = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev",
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
-  },
-];
 
 import { defineComponent, ref } from "vue";
 // import navbar from "../components/navbar.vue";
@@ -115,14 +80,9 @@ export default defineComponent({
   },
 
   setup() {
-    const leftDrawerOpen = ref(false);
 
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
+
     };
   },
 });
