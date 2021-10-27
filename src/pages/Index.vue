d6cbf4a6047c2844c171a4cab723223548785bfc
austin4399 <Austin4399@gmail.com>
2021-10-27 16:17:32 -0400

    added carousel with rounded borders

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/FABlogo.png"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+
+      <div class="q-pa-md">
+            <div class="q-pa-md">
+              <q-carousel
+                animated
+                v-model="slide"
+                arrows
+                navigation
+                infinite
+                class="rounded-borders"
+              >
+                <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+                <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+                <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+                <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+              </q-carousel>
+            </div>
+    </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'PageIndex'
-})
+export default {
+  setup () {
+    return {
+      slide: ref(1)
+    }
+  }
+}
 </script>
