665d8d8aead87e8a5ca6b32bf1ef564fd69e33ef
mo17817 <lazerharry@gmail.com>
2021-11-05 16:53:20 -0400

    formatting

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,11 @@
 <template>
+<head>
+  <link rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Raleway">
+</head>
 <div>
   <navbar></navbar>
-  <q-page class="bg-secondary-5">
+  <q-page class="bg-black">
     <!-- <q-img
         id="banner"      
         src="~assets/cleaning2.jpeg"
@@ -9,24 +13,49 @@
 
     </q-img> -->
   
-  
+  <div class="col">
+    <div class="col">
+      <div id="carousel-container">
+          <carousel></carousel>
+      </div>
+    </div>
+  </div>
 
-  <div class="q-pa-md row">
+
+
+  <div class="q-pa-md row absolute-center" id="text-container">
+
 
     <div class="col">
       <div class="col" id="banner-text-container">
-        <h2 class="text-h3 text-weight-light text-center text-black">FAB cleaning</h2>
+        <h2 class="text-h3 text-weight-light text-center text-white">FAB cleaning</h2>
       </div>
 
       <div class="col" id="banner-subtext-container">
-        <h3 class="text-h2 text-center text-black">Like a breath-mint for the whip</h3>
-          <carousel></carousel>
-      <h1 class = "text-h2 text-center text-black"> Check out more of our services and plans below </h1>
-        
-    </div>
-
+        <h3 class="text-h2 text-center text-weight-light text-white">Like a breath-mint for the whip</h3>
+        <h1 class = "text-h2 text-center text-white"> Come see what we can do for you! </h1>
+      
+      </div>
     </div>
   </div>
+
+  <div class="col" id="card-container">
+    <q-card dark class="bg-secondary my-card text-black">
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-separator dark inset />
+
+      <q-card-section>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </q-card-section>
+    </q-card>
+  </div>
+
+    
   </q-page>
 </div>
 </template>
@@ -58,17 +87,26 @@ export default {
   z-index: -1;
 }
 
-.q-page{
-  z-index: -1;
+#carousel-container{
+  padding: 0;
+  margin: 0;
+
 }
-.my-card{
-  width: 100%;
-  max-width: 400px;
-  height: 800px;
+
+#text-container{
+  top: 30vh;
+  display: flex;
+  flex-direction: column;
+  // align-self: center;
+  margin-bottom: 10%;
+  font-family: Raleway, serif;
+  text-shadow: 4px 4px 4px #aaa;
 }
-.index_infocard{
-  width: 30%;
-  padding: 25%;
-  margin: 25%;
+
+#card-container{
+  margin-top: 5%;
+  margin-left: 5%;
+  margin-right: 5%;
+  
 }
 </style>
