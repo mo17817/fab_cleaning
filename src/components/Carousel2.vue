bc11af2b1caee2b1c25ff196266e75e7de64c110
mo17817 <lazerharry@gmail.com>
2021-11-06 17:37:52 -0400

    carousel for the about me page

---
+++ src/components/Carousel2.vue
@@ -0,0 +1,41 @@
+<template>
+<q-carousel id ="carousel"
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      :autoplay="autoplay"
+      arrows
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+      height="500px"
+      dark
+      arows
+    >
+      <q-carousel-slide :name="1" img-src="~assets/cleaning1.jpeg"/>
+      <q-carousel-slide :name="2" img-src="~assets/cleaning2.jpeg" />
+      <q-carousel-slide :name="3" img-src="https://c.pxhere.com/photos/34/c8/car_mercedes_slk_auto_transport_design_transportation_luxury-1382710.jpg!d"  />
+      <q-carousel-slide :name="4" img-src="~assets/cleaning4.jpeg" />
+    </q-carousel>
+  </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      slide: ref(1),
+      autoplay: ref(true)
+    }
+  }
+}
+</script>
+<style>
+    #carousel{
+        margin-right: 0;
+    }
+
+</style>
