72d5b95006f22dda8b6a624d11c7e807d5f14d12
mo17817 <lazerharry@gmail.com>
2021-11-10 00:06:37 -0500

    no changes

---
+++ src/components/Infocard3.vue
@@ -0,0 +1,28 @@
+<template>
+    <q-card dark bordered class="bg-black my-card text-white">
+      <q-card-section>
+        <q-card-actions align = "left">
+        <div class="text-h6">Our Mission Statement </div>
+          <q-space />
+        <div class="text-subtitle2"> FAB cleaning</div>
+        </q-card-actions>
+      </q-card-section>
+
+
+      <q-separator dark inset/>
+
+      <q-card-section>
+      </q-card-section>
+    </q-card>
+</template>
+<script>
+export default {
+  setup () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+
+</style>
