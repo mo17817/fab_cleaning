713e6f5df0a7a3dae700d7a3328735ff4087e23d
austin4399 <Austin4399@gmail.com>
2021-11-20 15:33:03 -0500

    component updates

---
+++ src/components/Horizontalcard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card" flat bordered
+    height = "300px">
+      <q-card-section horizontal>
+        <q-card-section>
+        </q-card-section>
+
+        <q-img
+          class="col-5"
+          src="https://cdn.quasar.dev/img/parallax2.jpg"
+        />
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section>
+        </q-card-section>
+
+        <q-separator vertical />
+
+        <q-card-section>
+            Testing
+        </q-card-section>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://cdn.quasar.dev/img/mountains.jpg"
+        />
+
+        <q-card-actions vertical class="justify-around q-px-md">
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
--- src/pages/services.vue
+++ src/pages/services.vue
@@ -1,30 +1,45 @@
 <template>
   <!-- <q-page> -->
-    <!--parallax needs to have a scroll parent element in it-->
-    <q-page>
-      <div class="q-pa-md q-gutter-md">
-        <q-page-scroller>
-        <div class="row justify-between">
-          <q-page-scroller>
-          <q-parallax
-           src="https://cdn.quasar.dev/img/parallax2.jpg"
-          
-           >
-            <h1 class="text-white">Basic</h1>
-          </q-parallax>
-        </div>
-      </q-page-scroller>
+  <!--parallax needs to have a scroll parent element in it-->
+  <q-page>
+    <div class="q-pa-md q-gutter-md">
+      <div class="row justify-between">
+        <q-parallax src="https://cdn.quasar.dev/img/parallax2.jpg">
+          <h1 class="text-white">Basic</h1>
+        </q-parallax>
       </div>
+      <q-card class="my-card" flat bordered>
+        <q-card-section>
+
+            <q-img
+              class="col-5"
+              src="~assets/goldtier.jpg"
+            />
+            <q-img
+              class="col-5"
+              src="~assets/silvertier.png"
+            />
+
+
+          <q-card-section>
+            testing the horizontal card section. Here we can see the text will
+            wrap in a strange manor
+          </q-card-section>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
   <!-- </q-page> -->
 </template>
 <script>
 import { defineComponent } from "@vue/composition-api";
 import { Parallax } from "components/Parallax.vue";
+// import { Horizontalcard } from "components/Horizontalcard.vue"
 export default defineComponent({
   setup() {},
   components: {
     // Parallax,
+    // Horizontalcard,
   },
 });
 </script>
