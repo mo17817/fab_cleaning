82b791b03d3d60a0e9c0a4cad45c3f2e1ac03f4a
mo17817 <lazerharry@gmail.com>
2021-11-24 01:37:44 -0500

    fixed the horizontal card

--- src/components/Horizontalcard.vue
+++ src/components/Horizontalcard.vue
@@ -12,33 +12,5 @@
         />
       </q-card-section>
     </q-card>
-
-    <q-card class="my-card" flat bordered>
-      <q-card-section horizontal>
-        <q-card-section>
-        </q-card-section>
-
-        <q-separator vertical />
-
-        <q-card-section>
-            Testing
-        </q-card-section>
-      </q-card-section>
-    </q-card>
-
-    <q-card class="my-card" flat bordered>
-      <q-card-section horizontal>
-        <q-img
-          class="col"
-          src="https://cdn.quasar.dev/img/mountains.jpg"
-        />
-
-        <q-card-actions vertical class="justify-around q-px-md">
-          <q-btn flat round color="red" icon="favorite" />
-          <q-btn flat round color="accent" icon="bookmark" />
-          <q-btn flat round color="primary" icon="share" />
-        </q-card-actions>
-      </q-card-section>
-    </q-card>
   </div>
 </template>
