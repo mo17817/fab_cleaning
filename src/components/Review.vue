cd13cca8bae527ab11f6703caed95b211f7aa478
mo17817 <lazerharry@gmail.com>
2021-12-03 15:58:54 -0500

    review component

---
+++ src/components/Review.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-5">
+    <q-card class="my-card" square bordered>
+      <q-card-actions vertical align="center">
+        <q-avatar rounded size="45px">
+          <q-img src = ~assets/5-star.png />
+        </q-avatar>
+        <h3>"We had a great experience at Fab Cleaning 5 stars!"</h3>
+      </q-card-actions>
+      <q-card-actions horizontal>
+          <q-btn align="left">
+              <q-icon name = "star_rate" color = "yellow-7"/>
+              <q-icon name = "star_rate" color = "yellow-7"/>
+              <q-icon name = "star_rate" color = "yellow-7"/>
+              <q-icon name = "star_rate" color = "yellow-7"/>
+              <q-icon name = "star_rate" color = "yellow-7"/>
+          </q-btn>
+      </q-card-actions>
+        <h3> Let us know what you think ! </h3>
+    </q-card>
+  </div>
+</template>
