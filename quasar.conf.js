aa96a967ccbb65845c4c1a205a23007c8f9e3eaf
austin4399 <Austin4399@gmail.com>
2021-12-04 16:44:53 -0500

    enabled dark mode

--- quasar.conf.js
+++ quasar.conf.js
@@ -83,7 +83,7 @@ module.exports = configure(function (ctx) {
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
       config: {
-        dark: false,
+        dark: true,
       },
 
       // iconSet: 'material-icons', // Quasar icon set
