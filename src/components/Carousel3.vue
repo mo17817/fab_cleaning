d4cb3765657114bbd20cf0275a66e0fc791aa881
mo17817 <lazerharry@gmail.com>
2021-12-08 01:33:27 -0500

    made a new carrousel for the review section

---
+++ src/components/Carousel3.vue
@@ -0,0 +1,34 @@
+<template>
+<q-carousel id ="carousel"
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      :autoplay="autoplay"
+      arrows
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+      height="250px"
+      dark
+      arows = "bg-black"
+    >
+      <q-carousel-slide :name="1" img-src="~assets/review.png" />
+      <q-carousel-slide :name="2" img-src="~assets/google-review.jpg" />
+      <q-carousel-slide :name="3" img-src />
+      <q-carousel-slide :name="4" img-src="~assets/cleaning4.jpeg" />
+    </q-carousel>
+  </template>
+  <script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      slide: ref(1),
+      autoplay: ref(true)
+    }
+  }
+}
+</script>
