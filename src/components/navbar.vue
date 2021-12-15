a5cf7413a4c9d99daf929984bd1c15223240ac4f
austin4399 <Austin4399@gmail.com>
2021-12-15 16:43:04 -0500

    mobile formatting

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -6,8 +6,8 @@
   </head>
   <q-header>
     <q-toolbar class="bg-black" id="navbar">
-        <div class="col-2">
-          <q-avatar size="56px">
+        <div class="col-3">
+          <q-avatar size="56px" class="q-my-sm">
             <img src="~assets/FABlogo.png" alt="Fab Logo" id="logo"/>
           </q-avatar>
         </div>
@@ -16,58 +16,107 @@
                 FAB Cleaning
           </div>
         </div>
-        <div class="col-2 text-primary" align="right">
-          <q-btn-dropdown class="text-primary bg-black"  color="primary" label= "Pages" id = "Dropdown">
-          <q-list class="bg-black" align="center">
-            <q-item>
-              <div class="col">
-                <q-btn
-                  name = "Home"
+        <div class="col-3 text-primary" align="right">
+          
+          <q-btn icon="menu">
+            <q-menu class="bg-black">
+              <q-list align="center">
+                <q-item class="text-primary">
+                  <div class="col">
+                    <q-btn
+                    to="/"
+                  >
+                    Home
+                  </q-btn>
+                  </div>
+                  
+                </q-item>
+                <q-item>
+                <div class="col">
+                  <q-btn
+                    class="text-primary"
+                    flat
+                    to="/services"
+                  >
+                    Services
+                  </q-btn>
+                </div>
+              </q-item>
+              <q-item>
+                <div class="col">
+                  <q-btn
                   class="text-primary"
-                  flat
-                  to="/"
-                  @click="updateDropdown(this.value)"
-                >
-                  Home
-                </q-btn>
-              </div>
-            </q-item>
-            <q-item>
-              <div class="col">
-                <q-btn
+                  flat 
+                  to = "/about"
+                  value="About"
+                  >
+                  About Me 
+                  </q-btn>
+                </div>
+              </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+          <!-- <q-icon color="primary">
+
+          <q-btn-dropdown class="text-primary bg-black"  color="primary" dropdown-icon="menu" id = "Dropdown">
+            <q-list class="bg-black" align="center">
+              <q-item>
+                <div class="col">
+                  <q-btn
+                    name = "Home"
+                    class="text-primary"
+                    flat
+                    to="/"
+                  >
+                    Home
+                  </q-btn>
+                </div>
+              </q-item>
+              <q-item>
+                <div class="col">
+                  <q-btn
+                    class="text-primary"
+                    flat
+                    to="/services"
+                  >
+                    Services
+                  </q-btn>
+                </div>
+              </q-item>
+              <q-item>
+                <div class="col">
+                  <q-btn
                   class="text-primary"
-                  flat
-                  to="/services"
-                >
-                  Services
-                </q-btn>
-              </div>
-            </q-item>
-            <q-item>
-              <div class="col">
-                <q-btn
-                class="text-primary"
-                flat 
-                to = "/about"
-                value="About"
-                @click="updateDropDown(this.value)"
-                >
-                About Me 
-                </q-btn>
-              </div>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
+                  flat 
+                  to = "/about"
+                  value="About"
+                  >
+                  About Me 
+                  </q-btn>
+                </div>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+          </q-icon> -->
       </div>
     </q-toolbar>
   </q-header>
 </template>
 
-<style>
-#Dropdown {
-  
+<style lang="css" scoped>
+#title{
+  font-size: xxx-large;
+}
+@media screen and (max-width: 400px) {
+  #title{
+    font-size: xx-large;
+  }
 }
 
+#Dropdown{
+  color: yellow;
+}
 </style>
 
 <script>
@@ -80,13 +129,7 @@
       }
     },
     methods: {
-      updateDropDown(value) {
-       alert(value);
-     }
     }
-  }
-  function test() {
-    console.log("test");
   }
 </script>
 
