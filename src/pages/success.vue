f44bc285da7c20518ce707c80ed9b7194cbe0195
austin4399 <Austin4399@gmail.com>
2021-12-30 14:41:53 -0500

    added success page for when form is submitted

---
+++ src/pages/success.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>
+      Your service request has been submitted. We will get back to you shortly
+    </h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
