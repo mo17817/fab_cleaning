a57a996ed083cdd5398bc35f9738baac2417de75
austin4399 <Austin4399@gmail.com>
2021-12-30 14:42:01 -0500

    added test page for demo

---
+++ src/pages/Test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="test-root">
+    <form ref="form" @submit.prevent="sendEmail">
+      <q-input name="name" label="Name" filled v-model="name" />
+      <q-input name="email" label="Email" filled v-model="email" />
+      <q-btn label="Submit" type="submit" color="primary" />
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import emailjs from "emailjs-com";
+
+export default {
+  setup() {
+    let name = ref(null);
+    let email = ref(null);
+
+    return { name, email };
+  },
+  methods: {
+    sendEmail() {
+      emailjs
+        .sendForm(
+          "service_pymn93q",
+          "template_ab123456789",
+          this.$refs.form,
+          "user_2PwSHZeOnQt3MqFWKqaKx"
+        )
+        .then(
+          (res) => {
+            console.log(res.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    },
+  },
+};
+</script>
